d7cc99b239d7223ebec62aade8a15320d6561d87
gtraiano <gtraiano@gmail.com>
2024-02-29 14:19:53 +0200

    renamed env variable

--- backend/src/controllers/houses/index.ts
+++ backend/src/controllers/houses/index.ts
@@ -1,6 +1,6 @@
 import { HousesResponse } from "../../../../types";
 
 export const fetchHouses = async (): Promise<HousesResponse> => {
-    const response = await fetch(process.env.BACKEND_FALLBACK_URL as string);
+    const response = await fetch(process.env.REMOTE_API_URL as string);
     return await response.json() as HousesResponse;
 }
